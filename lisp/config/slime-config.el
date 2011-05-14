c4b7a2c6f21139731b20638451c6d896257061ac
thomas <thomas@aspire-linux.(none)>
2011-05-14 09:02:49 +0200

    # connect name extension *.cl with lisp-mode

--- lisp/config/slime-config.el
+++ lisp/config/slime-config.el
@@ -8,3 +8,6 @@
 ;; (add-to-list 'load-path "_the path of your `slime' directory_")
 (require 'slime-autoloads)
 (slime-setup)
+
+;; add file extension '*.cl'
+(add-to-list 'auto-mode-alist '("\\.cl\\'" . lisp-mode))
