224ba448057c970dbdae5b8e3cfc072e75dad339
Thomas Pröger <proeger@cbs.mpg.de>
2011-05-18 18:56:42 +0200

    configured cedet support to run the current CEDET version that comes along with emacs23.2

--- lisp/config/cedet-config.el
+++ lisp/config/cedet-config.el
@@ -4,20 +4,23 @@
 ;; CEDET - configuration file
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; a cedet howto can be found at:
+;; a DEPRECATED cedet howto can be found at:
 ;; http://xtalk.msk.su/~ott/en/writings/emacs-devenv/EmacsCedet.html
 
 ;; (loads file cedet.el)
-(require 'cedet)
+;; (require 'cedet)
 
 ;; enable project management
 ;; user defined projects will be declared at the end of the file
 (global-ede-mode t)
 
+;; enable semantic support
+(semantic-mode t)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; my projects
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(if (file-exists-p "~/files/programming/isis/README")
-    (ede-cpp-root-project "isis" :file "~/files/programming/isis/README"
-			  :name "ISIS"))
+;; (if (file-exists-p "~/files/programming/isis/README")
+;;     (ede-cpp-root-project "isis" :file "~/files/programming/isis/README"
+;; 			  :name "ISIS"))
